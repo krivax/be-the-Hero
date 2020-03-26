06b8c03306bf02da45fbebb46c92e5256be35deb
Krivax <krivahz@gmail.com>
2020-03-26 12:56:56 -0300

    Frontend do App

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -20,7 +20,7 @@ module.exports = {
 
     async create(request, response) {
         const { title, description, value } = request.body;
-        const ong_id = request.headers.autorization;
+        const ong_id = request.headers.authorization;
 
         const [id] = await connection('incidents').insert({
             title,
